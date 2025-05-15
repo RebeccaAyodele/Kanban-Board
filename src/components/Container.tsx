88ac2baa49f4f58684d0ebafe0b99b0634cf2689
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-05-15 23:49:50 +0100

    Added the functionality of the add button and ibput task but Ive not added every of its value

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,5 @@
 import { AddTaskIcon } from "./Icons";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useStore } from "../store";
 import Card from "./Card";
 
@@ -10,25 +10,36 @@ type Props = {
 };
 
 const Container = ({ state, heading, description }: Props) => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
   const tasks = useStore((store) => store.tasks);
   const filtered = useMemo(
     () => tasks.filter((task) => task.state === state),
     [tasks, state]
   );
 
-  const addTask = useStore((store) => store.addTask)
+  const addTask = useStore((store) => store.addTask);
 
   return (
     <div className="min-h-[706px] w-[28%] bg-primary-color rounded-[1.4rem] pt-[2.6rem] px-[2rem] mt-8 flex flex-col">
       <div className="flex justify-between items-center">
         <h1 className="text-[22px] font-semibold">{state}</h1>
-        <button
-          onClick={() => {
-            addTask("chbbcd", state);
-          }}
-        >
-          <AddTaskIcon />
-        </button>
+        <button onClick={() => setOpen(true)}> <AddTaskIcon /></button>
+
+        {open && (
+          <div className="absolute w-full h-full top-0 left-0">
+            <div className="bg-blue-300 absolute z-1 p-4 h-screen/2 w-screen/2 left-1/3 top-1/2 translate-x-1/2 translate-y-1/2 flex justify-center items-center">
+              <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+            onClick={() => {
+              addTask(text, state);
+              setText("");
+              setOpen(false);
+            }}
+          >Add</button>
+            </div>
+          </div>
+        )}
       </div>
       <div className="border-[#81C3FF] w-[98%] h-[275px] mt-[4rem] rounded-3xl border-dotted border-2 flex flex-col justify-center items-center">
         <h1 className="font-bold text-gray-700 text-[14px]">{heading}</h1>
